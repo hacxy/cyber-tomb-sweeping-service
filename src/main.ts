aa04717720854866df93d501ea612cb2c1072efc
Hacxy <hacxy.97@outlook.com>
2024-04-04 03:50:51 +0800

    fix: 尝试修复跨域

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ bootstrap(AppModule, {
       origin: '*',
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
       allowedHeaders: [
+        'Access-Control-Allow-Origin',
         'Content-Type',
         'Origin',
         'X-Requested-With',
